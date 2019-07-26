61ba863c4a094ab3fae9e3561de2219ee5cbfc0e
RamyaSakthi <53175031+RamyaSakthi@users.noreply.github.com>
2019-07-26 14:04:10 +0530

    Create l1s14.c

---
+++ l1s14.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if(a>='a'&&a<='z')
+    {
+        printf("Alphabet");
+    }
+    else
+    {
+        printf("Not");
+    }
+    return 0;
+}
