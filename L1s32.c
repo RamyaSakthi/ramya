102f6fc02432750136fc8a21915012f9ab4821a2
RamyaSakthi <53175031+RamyaSakthi@users.noreply.github.com>
2019-07-29 14:44:56 +0530

    Create L1s32.c

---
+++ L1s32.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[10],i,j,m;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    m=0;
+    for(i=0;i<n;i++)
+    {
+    if(a[i]>m)
+    {
+        m=a[i];
+    }
+    }
+    printf("%d",m);
+    
+   return 0;
+}
